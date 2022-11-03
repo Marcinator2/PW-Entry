dd15a12fa75bb4f1ffbeabb53e83a67fc96a50a2
Marcinator_2 <Marcinator_2@DESKTOP-50S2LSH>
2022-11-03 18:10:48 +0100

    Projektdateien hinzufügen.

---
+++ Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace PW_Entry
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.tbxDelay = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbxWartezeit = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbxPasswd = new System.Windows.Forms.TextBox();
+            this.btnPasswort = new System.Windows.Forms.Button();
+            this.lbxListe = new System.Windows.Forms.ListBox();
+            this.btnListeimportieren = new System.Windows.Forms.Button();
+            this.pageSetupDialog1 = new System.Windows.Forms.PageSetupDialog();
+            this.btnFensterSuchen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 194);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Delay in ms:";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(31, 241);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(153, 79);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // tbxDelay
+            // 
+            this.tbxDelay.Location = new System.Drawing.Point(31, 213);
+            this.tbxDelay.Name = "tbxDelay";
+            this.tbxDelay.Size = new System.Drawing.Size(70, 22);
+            this.tbxDelay.TabIndex = 2;
+            this.tbxDelay.Text = "250";
+            this.tbxDelay.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tbxDelay.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbxDelay_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Wartezeit in Sekunden";
+            // 
+            // tbxWartezeit
+            // 
+            this.tbxWartezeit.Location = new System.Drawing.Point(31, 169);
+            this.tbxWartezeit.Name = "tbxWartezeit";
+            this.tbxWartezeit.Size = new System.Drawing.Size(131, 22);
+            this.tbxWartezeit.TabIndex = 4;
+            this.tbxWartezeit.Text = "2";
+            this.tbxWartezeit.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tbxWartezeit.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbxWartezeit_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Passwort eingeben:";
+            // 
+            // tbxPasswd
+            // 
+            this.tbxPasswd.Location = new System.Drawing.Point(31, 64);
+            this.tbxPasswd.Name = "tbxPasswd";
+            this.tbxPasswd.Size = new System.Drawing.Size(153, 22);
+            this.tbxPasswd.TabIndex = 6;
+            this.tbxPasswd.UseSystemPasswordChar = true;
+            // 
+            // btnPasswort
+            // 
+            this.btnPasswort.Location = new System.Drawing.Point(31, 103);
+            this.btnPasswort.Name = "btnPasswort";
+            this.btnPasswort.Size = new System.Drawing.Size(153, 35);
+            this.btnPasswort.TabIndex = 7;
+            this.btnPasswort.Text = "Passwort anzeigen";
+            this.btnPasswort.UseVisualStyleBackColor = true;
+            this.btnPasswort.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnPasswort_MouseDown);
+            this.btnPasswort.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btnPasswort_MouseUp);
+            // 
+            // lbxListe
+            // 
+            this.lbxListe.FormattingEnabled = true;
+            this.lbxListe.ItemHeight = 16;
+            this.lbxListe.Location = new System.Drawing.Point(207, 92);
+            this.lbxListe.Name = "lbxListe";
+            this.lbxListe.Size = new System.Drawing.Size(162, 228);
+            this.lbxListe.TabIndex = 9;
+            this.lbxListe.SelectedIndexChanged += new System.EventHandler(this.lbxListe_SelectedIndexChanged);
+            // 
+            // btnListeimportieren
+            // 
+            this.btnListeimportieren.Location = new System.Drawing.Point(207, 57);
+            this.btnListeimportieren.Name = "btnListeimportieren";
+            this.btnListeimportieren.Size = new System.Drawing.Size(162, 29);
+            this.btnListeimportieren.TabIndex = 10;
+            this.btnListeimportieren.Text = "Liste importieren";
+            this.btnListeimportieren.UseVisualStyleBackColor = true;
+            this.btnListeimportieren.Click += new System.EventHandler(this.btnListeImportieren_Click);
+            // 
+            // btnFensterSuchen
+            // 
+            this.btnFensterSuchen.Location = new System.Drawing.Point(207, 15);
+            this.btnFensterSuchen.Name = "btnFensterSuchen";
+            this.btnFensterSuchen.Size = new System.Drawing.Size(162, 32);
+            this.btnFensterSuchen.TabIndex = 11;
+            this.btnFensterSuchen.Text = "Fenster suchen";
+            this.btnFensterSuchen.UseVisualStyleBackColor = true;
+            this.btnFensterSuchen.Click += new System.EventHandler(this.btnFensterSuchen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 332);
+            this.Controls.Add(this.btnFensterSuchen);
+            this.Controls.Add(this.btnListeimportieren);
+            this.Controls.Add(this.lbxListe);
+            this.Controls.Add(this.btnPasswort);
+            this.Controls.Add(this.tbxPasswd);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbxWartezeit);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbxDelay);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "PW-Entry";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox tbxDelay;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbxWartezeit;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbxPasswd;
+        private System.Windows.Forms.Button btnPasswort;
+        private System.Windows.Forms.ListBox lbxListe;
+        private System.Windows.Forms.Button btnListeimportieren;
+        private System.Windows.Forms.PageSetupDialog pageSetupDialog1;
+        private System.Windows.Forms.Button btnFensterSuchen;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using WindowsInput.Native;
+using WindowsInput;
+using System.Reflection;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+
+
+namespace PW_Entry
+{
+    public partial class Form1 : Form
+    {
+        internal const int ActiveResize = 3;
+
+        //Import the FindWindow API to find our window
+        [DllImportAttribute("User32.dll")]
+        private static extern int FindWindow(String ClassName, String WindowName);
+
+        //Import the SetForeground API to activate it
+        [DllImportAttribute("User32.dll")]
+        private static extern IntPtr SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool ShowWindow(IntPtr hWnd1, int nCmdShow);
+
+        List<string> PWListe = new List<string>();
+        List<string> IPListe = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnListeImportieren_Click(sender, e);
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+
+            int Wartezeit = Int32.Parse(tbxWartezeit.Text);
+            int Delay = Int32.Parse(tbxDelay.Text);
+
+            //string Passwort = tbxPasswd.Text;
+            //    string Passwort1 = listView1.SelectedItems+1.ToString();    
+            string Passwort = lbxListe.SelectedIndex.ToString();
+
+            Passwort = tbxPasswd.Text;
+            Wartezeit = Wartezeit * 1000;
+
+            simulateTypingText(Passwort, Delay, Wartezeit);
+            //btnStart.BackColor = Color.Orange;
+
+        }
+
+
+
+        public void simulateTypingText(string Text, int typingDelay = 100, int startDelay = 0)
+        {
+            //btnStart.BackColor = Color.Orange;
+            InputSimulator sim = new InputSimulator();
+
+            // Wait the start delay time
+            sim.Keyboard.Sleep(startDelay);
+
+            // Split the text in lines in case it has
+            string[] lines = Text.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None);
+
+            foreach (string line in lines)
+            {
+                // Split line into characters
+                char[] words = line.ToCharArray();
+
+                // Simulate typing of the char i.e: a, e , i ,o ,u etc
+                // Apply immediately the typing delay
+                foreach (char word in words)
+                {
+                    sim.Keyboard.TextEntry(word).Sleep(typingDelay);
+                }
+
+                //Enter nach PW drücken
+                sim.Keyboard.KeyPress(VirtualKeyCode.RETURN);
+
+            }
+        }
+
+        private void btnPasswort_MouseDown(object sender, MouseEventArgs e)
+        {
+            tbxPasswd.UseSystemPasswordChar = false;
+        }
+
+        private void btnPasswort_MouseUp(object sender, MouseEventArgs e)
+        {
+            tbxPasswd.UseSystemPasswordChar = true;
+        }
+        private void tbxDelay_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+
+            }
+        }
+        private void tbxWartezeit_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+
+            }
+        }
+
+        public void btnListeImportieren_Click(object sender, EventArgs e)
+
+        {
+            try
+            {
+            lbxListe.Items.Clear();
+            PWListe.Clear();
+            IPListe.Clear();
+            //PWListe.Items.Clear();
+            //var path = @"D:\New folder\Data.csv";
+            var pfad = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            string path = pfad + @"\Data.csv";
+
+                List<example> values = File.ReadAllLines(path)
+
+                                              .Skip(1)
+
+                                              .Select(v => example.FromCsv(v))
+
+                                              .ToList();
+
+                foreach (var item in values)
+
+                {
+
+                    lbxListe.Items.Add(item.t1);
+                    PWListe.Add(item.t2);
+                    IPListe.Add(item.t3);
+
+                }
+            }
+            catch (Exception ex)
+            { MessageBox.Show("Data.csv ist geöffnet oder nicht vorhanden."); }
+            return;
+        }
+
+        public class example
+
+        {
+
+            public string t1;
+
+            public string t2;
+
+            public string t3;
+
+            public static example FromCsv(string csvLine)
+
+            {
+
+                string[] values = csvLine.Split(';');
+
+                example dailyValues = new example();
+
+                dailyValues.t1 = values[0];
+                dailyValues.t2 = values[1];
+                dailyValues.t3 = values[2];
+
+
+                return dailyValues;
+
+            }
+
+        }
+
+        private void lbxListe_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lbxListe.SelectedIndex >= 0)
+            {
+                tbxPasswd.Text = PWListe[lbxListe.SelectedIndex];
+
+            }
+        }
+
+      
+        private void btnFensterSuchen_Click(object sender, EventArgs e)
+        {
+
+            Process[] processlist = Process.GetProcesses();
+
+            
+
+            foreach (Process process in processlist)
+            {
+
+                if (!String.IsNullOrEmpty(process.MainWindowTitle) && lbxListe.SelectedIndex >= 0 && Convert.ToString(IPListe[lbxListe.SelectedIndex]) != "")
+                {
+                    if (process.MainWindowTitle.Contains(IPListe[lbxListe.SelectedIndex]))
+                    {
+                        //MessageBox.Show(IPListe[lbxListe.SelectedIndex]);
+                        ShowWindow(process.MainWindowHandle, 9);
+                        SetForegroundWindow(process.MainWindowHandle);
+                        
+                    }
+                }
+            }
+
+        }
+     
+
+    }
+
+
+}
+
